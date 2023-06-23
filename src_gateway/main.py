dcc8458f0a303c694cb4f3c89386fe6a234f6356
Jimon-s <22317839+Jimon-s@users.noreply.github.com>
2023-06-24 08:05:53 +0900

    chore: Add typing-requests to resolve type error

--- src_gateway/main.py
+++ src_gateway/main.py
@@ -1,3 +1,10 @@
-import sys
-def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
+from typing import Any, Dict, cast
+
+from slack_bolt import App
+from slack_bolt.adapter.aws_lambda import SlackRequestHandler
+
+app = App(process_before_response=True)
+
+def handler(event: Dict[str, Any] , context: Dict[str,Any]) -> Dict[str, Any]:
+    slack_handler=SlackRequestHandler(app=app)
+    return cast(Dict[str, Any], slack_handler.handle(event=event,context=context) ) 
